e008dbbaa270a55fd6259816f9bb28fed3c89c9e
developerr-ayush <developerr.ayush@gmail.com>
2024-10-02 21:19:47 +0530

    nextjs

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,7 @@ import { Contact } from "@/components/contact";
 export default function contact() {
   return (
     <div>
-      <Hero img="/image/FAQ.jpeg" />
+      <Hero img="/image/contact.jpeg" />
       <div className="max-w-[60rem] mx-auto px-6 py-20">
         <Contact />
       </div>
