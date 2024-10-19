7cdff08ef279c25b7e54c6771a8eefee8946c0b7
developerr-ayush <developerr.ayush@gmail.com>
2024-10-19 20:46:54 +0530

    fix

--- components/years-exp/index.tsx
+++ components/years-exp/index.tsx
@@ -7,7 +7,7 @@ export const YearsExp = () => {
   return (
     <SectionWrapper className=" YearsExp">
       <GSAP />
-      <Title className="mx-auto text-[2rem] lg:text-[3.125rem] text-[#bb8866] mb-2 text-center ">
+      <Title className="mx-auto  text-[2rem] lg:text-[3.125rem] text-[#bb8866] mb-12 text-center ">
         Values Etched In Veracity
       </Title>
 
@@ -21,7 +21,7 @@ export const YearsExp = () => {
             <span className="shrink-0">2</span>
           </h2>
         </div>
-        <h2 className="flex flex-col pb-7 gap-2 text-[1.4rem] md:text-[3rem] md:pb-[3.5rem] leading-none textual-context tracking-[3.5px]">
+        <h2 className="flex flex-col pb-7 gap-2 text-[1.8rem] md:text-[5rem] md:pb-[3.5rem] leading-none textual-context tracking-[3.5px]">
           <span className="  text-[#bb8866]">years of legacy</span>
         </h2>
       </div>
