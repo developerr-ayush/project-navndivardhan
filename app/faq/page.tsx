15a403279a94a8ccbc581bf98dd814768fc8b310
developerr-ayush <developerr.ayush@gmail.com>
2024-10-19 13:23:03 +0530

    fix

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -3,144 +3,37 @@ import { Hero } from "@/components/hero";
 import React from "react";
 const faq = {
   title: "Frequently Asked Questions",
-  subTitle:
-    "Find quick answers to your most common questions about our services and policies right here",
   list: [
     {
-      question:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facer mollitia atque",
-      answer: ` Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-                mollitia atque unde fugit rem impedit distinctio nisi nihil fuga
-                velit, id porro voluptatum totam, tempore cum ad dolorem
-                voluptates, veritatis aliquam? Corporis autem architecto nulla
-                quibusdam. Quibusdam reprehenderit soluta ipsam consequatur nam
-                repellat similique vel sint ut consequuntur libero enim, officiis
-                iure deserunt aut quisquam voluptatibus iusto tempore culpa quasi
-                eligendi asperiores mollitia commodi omnis. Excepturi voluptates
-                blanditiis voluptas corporis suscipit, minus illo laboriosam
-                possimus rem atque, perspiciatis quaerat corrupti itaque voluptate
-                dicta saepe iusto modi vel, hic magni molestias. Sequi
-                exercitationem corporis accusamus hic consequatur ducimus sed,
-                vitae laudantium!`,
+      question: "What documents are needed for the redevelopment process?",
+      html: `
+      Conveyance of the property <br>
+Property Card in the name of the Society<br>
+Old Occupation Certificate and Plans<br>
+CTS Plan<br>
+List of Allotment of Members with Carpet Area<br>
+Development Plan Remark<br>
+      `,
     },
     {
-      question:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facer mollitia atque",
-      answer: ` Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-                mollitia atque unde fugit rem impedit distinctio nisi nihil fuga
-                velit, id porro voluptatum totam, tempore cum ad dolorem
-                voluptates, veritatis aliquam? Corporis autem architecto nulla
-                quibusdam. Quibusdam reprehenderit soluta ipsam consequatur nam
-                repellat similique vel sint ut consequuntur libero enim, officiis
-                iure deserunt aut quisquam voluptatibus iusto tempore culpa quasi
-                eligendi asperiores mollitia commodi omnis. Excepturi voluptates
-                blanditiis voluptas corporis suscipit, minus illo laboriosam
-                possimus rem atque, perspiciatis quaerat corrupti itaque voluptate
-                dicta saepe iusto modi vel, hic magni molestias. Sequi
-                exercitationem corporis accusamus hic consequatur ducimus sed,
-                vitae laudantium!`,
+      question: "How are buyers protected during redevelopment?",
+      answer:
+        "The Real Estate Regulation and Development Act of 2016 ensures protection for home buyers. Grievances can be directed to MAHARERA for resolution.",
     },
     {
-      question:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facer mollitia atque",
-      answer: ` Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-                mollitia atque unde fugit rem impedit distinctio nisi nihil fuga
-                velit, id porro voluptatum totam, tempore cum ad dolorem
-                voluptates, veritatis aliquam? Corporis autem architecto nulla
-                quibusdam. Quibusdam reprehenderit soluta ipsam consequatur nam
-                repellat similique vel sint ut consequuntur libero enim, officiis
-                iure deserunt aut quisquam voluptatibus iusto tempore culpa quasi
-                eligendi asperiores mollitia commodi omnis. Excepturi voluptates
-                blanditiis voluptas corporis suscipit, minus illo laboriosam
-                possimus rem atque, perspiciatis quaerat corrupti itaque voluptate
-                dicta saepe iusto modi vel, hic magni molestias. Sequi
-                exercitationem corporis accusamus hic consequatur ducimus sed,
-                vitae laudantium!`,
+      question: "What approvals are necessary for redevelopment?",
+      answer:
+        "Key approvals include NOCs from various departments (e.g., Fire, Traffic, BMC) and the Building Proposal Department.",
     },
     {
-      question:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facer mollitia atque",
-      answer: ` Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-                mollitia atque unde fugit rem impedit distinctio nisi nihil fuga
-                velit, id porro voluptatum totam, tempore cum ad dolorem
-                voluptates, veritatis aliquam? Corporis autem architecto nulla
-                quibusdam. Quibusdam reprehenderit soluta ipsam consequatur nam
-                repellat similique vel sint ut consequuntur libero enim, officiis
-                iure deserunt aut quisquam voluptatibus iusto tempore culpa quasi
-                eligendi asperiores mollitia commodi omnis. Excepturi voluptates
-                blanditiis voluptas corporis suscipit, minus illo laboriosam
-                possimus rem atque, perspiciatis quaerat corrupti itaque voluptate
-                dicta saepe iusto modi vel, hic magni molestias. Sequi
-                exercitationem corporis accusamus hic consequatur ducimus sed,
-                vitae laudantium!`,
+      question: "Who can opt for redevelopment?",
+      answer:
+        "Any building that has not fully utilized its plot potential under current DC regulations can consider redevelopment, subject to clear title.",
     },
     {
-      question:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facer mollitia atque",
-      answer: ` Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-                mollitia atque unde fugit rem impedit distinctio nisi nihil fuga
-                velit, id porro voluptatum totam, tempore cum ad dolorem
-                voluptates, veritatis aliquam? Corporis autem architecto nulla
-                quibusdam. Quibusdam reprehenderit soluta ipsam consequatur nam
-                repellat similique vel sint ut consequuntur libero enim, officiis
-                iure deserunt aut quisquam voluptatibus iusto tempore culpa quasi
-                eligendi asperiores mollitia commodi omnis. Excepturi voluptates
-                blanditiis voluptas corporis suscipit, minus illo laboriosam
-                possimus rem atque, perspiciatis quaerat corrupti itaque voluptate
-                dicta saepe iusto modi vel, hic magni molestias. Sequi
-                exercitationem corporis accusamus hic consequatur ducimus sed,
-                vitae laudantium!`,
-    },
-    {
-      question:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facer mollitia atque",
-      answer: ` Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-                mollitia atque unde fugit rem impedit distinctio nisi nihil fuga
-                velit, id porro voluptatum totam, tempore cum ad dolorem
-                voluptates, veritatis aliquam? Corporis autem architecto nulla
-                quibusdam. Quibusdam reprehenderit soluta ipsam consequatur nam
-                repellat similique vel sint ut consequuntur libero enim, officiis
-                iure deserunt aut quisquam voluptatibus iusto tempore culpa quasi
-                eligendi asperiores mollitia commodi omnis. Excepturi voluptates
-                blanditiis voluptas corporis suscipit, minus illo laboriosam
-                possimus rem atque, perspiciatis quaerat corrupti itaque voluptate
-                dicta saepe iusto modi vel, hic magni molestias. Sequi
-                exercitationem corporis accusamus hic consequatur ducimus sed,
-                vitae laudantium!`,
-    },
-    {
-      question:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facer mollitia atque",
-      answer: ` Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-                mollitia atque unde fugit rem impedit distinctio nisi nihil fuga
-                velit, id porro voluptatum totam, tempore cum ad dolorem
-                voluptates, veritatis aliquam? Corporis autem architecto nulla
-                quibusdam. Quibusdam reprehenderit soluta ipsam consequatur nam
-                repellat similique vel sint ut consequuntur libero enim, officiis
-                iure deserunt aut quisquam voluptatibus iusto tempore culpa quasi
-                eligendi asperiores mollitia commodi omnis. Excepturi voluptates
-                blanditiis voluptas corporis suscipit, minus illo laboriosam
-                possimus rem atque, perspiciatis quaerat corrupti itaque voluptate
-                dicta saepe iusto modi vel, hic magni molestias. Sequi
-                exercitationem corporis accusamus hic consequatur ducimus sed,
-                vitae laudantium!`,
-    },
-    {
-      question:
-        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facer mollitia atque",
-      answer: ` Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere
-                mollitia atque unde fugit rem impedit distinctio nisi nihil fuga
-                velit, id porro voluptatum totam, tempore cum ad dolorem
-                voluptates, veritatis aliquam? Corporis autem architecto nulla
-                quibusdam. Quibusdam reprehenderit soluta ipsam consequatur nam
-                repellat similique vel sint ut consequuntur libero enim, officiis
-                iure deserunt aut quisquam voluptatibus iusto tempore culpa quasi
-                eligendi asperiores mollitia commodi omnis. Excepturi voluptates
-                blanditiis voluptas corporis suscipit, minus illo laboriosam
-                possimus rem atque, perspiciatis quaerat corrupti itaque voluptate
-                dicta saepe iusto modi vel, hic magni molestias. Sequi
-                exercitationem corporis accusamus hic consequatur ducimus sed,
-                vitae laudantium!`,
+      question: "How to choose the right developer?",
+      answer:
+        "Evaluate a developer's financial stability, past performance, and compliance with statutory regulations.",
     },
   ],
 };
